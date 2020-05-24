50f751bb126bc91b9786d8ccd273494527032c78
Ajay <ajajaj2807@gmail.com>
2020-05-24 16:31:10 +0530

    footer text change

--- comps/Footer.js
+++ comps/Footer.js
@@ -2,8 +2,13 @@ const Footer = () => {
   return (
     <div className="footer-wrapper">
       <span className="footer-thanks">Thank You for visiting!</span>
-      <span className="f-mb">Made with <span style={{'color':'red'}}> ❤ </span> by Ajay Yadav</span>
-      <span className="f-sl">Made from scratch in React</span>
+      <span className="f-mb">
+        Made with <span style={{ color: "red" }}> ❤ </span> by Ajay Yadav
+      </span>
+      <span className="f-sl">
+        Made from scratch in React
+        <br />I have a project idea. Contact me if interested.
+      </span>
     </div>
   );
 };
