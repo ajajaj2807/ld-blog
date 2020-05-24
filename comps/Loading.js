80fefea54df2fd0bd1e0dd8a8f04a03dffecf968
Ajay <ajajaj2807@gmail.com>
2020-05-24 16:57:10 +0530

    Facebook, Twitter SEO added

--- comps/Loading.js
+++ comps/Loading.js
@@ -1,11 +1,6 @@
-import Head from 'next/head'
-
 const Loading = () => {
   return (
     <div className="loading-wrapper">
-      <Head>
-        <title>Loading...</title>
-      </Head>
       <div className="loader"></div>
       Loading...
     </div>
